ec05aa2e0fba4106b513f82fc13c4cd6fba8ee02
NightingaleWK <djsywxsj@outlook.com>
2025-04-03 17:26:45 +0800

    进度更新

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def root():
 @app.get("/items/")
 async def read_items(item: str | None = None, size: int | None = None):
     return {"item": item, "size": size}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
