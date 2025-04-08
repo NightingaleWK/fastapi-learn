4a1c7a0daba5f0afcb98467946def2690a1d103a
NightingaleWK <djsywxsj@outlook.com>
2025-04-08 11:26:41 +0800

    学习进度更新

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 from fastapi import FastAPI
-from enum import Enum
+from pydantic import BaseModel
 
-class ModelName(str,Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-@app.get("/items/")
-async def read_items(item: str, size: int):
-    return {"item": item, "size": size}
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
